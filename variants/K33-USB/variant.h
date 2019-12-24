5df942b3ee804156403e376adcf7c0f7af5ef4c5
px4ugs <px4ugs@users.noreply.github.com>
2019-12-24 11:13:02 +0200

    DOSFS removed

--- variants/K33-USB/variant.h
+++ variants/K33-USB/variant.h
@@ -83,7 +83,6 @@ extern "C"
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
 static const uint8_t A2  = PIN_A2;
-static const uint8_t DAC0 = PIN_DAC0;
 
 #define ADC_RESOLUTION          12
 
